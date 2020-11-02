216c09197b1b332b151dec8961573157b6851833
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-02 18:25:47 +0530

    Modified the Cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-const cors = require("cors")
 
 dotenv.config();
 
@@ -10,8 +9,6 @@ const app = express()
 const routers = require("./Routes/Routes")
 
 app.use(express.json())
-
-app.use(cors())
 
 mongoose.connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
