8e0952fa171a5eeea2ea9c4bc53fcfc456e27f62
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-02 16:47:49 +0530

    initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            NavBar
+            <button>SignUp</button>
+            <button>login</button>
+        </div>
+    )
+}
---
+++ src/Components/Pages/BBStar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BBStar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
