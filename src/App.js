ed0ff2ce36e60b07fc9c20c1a1c1be59d5ca0a34
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-03 18:23:25 +0530

    Created Product page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react"
-import BBStart from "./Components/Pages/BBStar"
+import { ProductItem } from "./Components/Pages/ProductItem"
 
 function App() {
+
+  const data = {
+    "productName": "Sugar",
+    "imageUrl": "https://www.bigbasket.com/media/uploads/p/l/40019396_8-bb-popular-sugar.jpg",
+    "brandName": "BB Popular",
+    "ratings": 4.0,
+    "reviews": 0,
+    "reviewList": [],
+    "subCategory": "Salt, sugar & Jaggery",
+    "category": "Foodgrains, oils & Masala",
+    "size": ["1 kg", "2 kg", "5 kg"],
+    "mrp": [55, 110, 275],
+    "offers": 20,
+    "description": "Almonds come in many varieties and the BB Royal California badam or almonds are premiere choice of many customers at bigbasket. The sweet and nutty taste of the almonds is combined here with hygienically packing to ensure freshness of these Californian Almonds. "
+  }
   return (
     <div>
-      <BBStart />
+      <ProductItem data={data} />
     </div>
   );
 }
---
+++ src/Components/Pages/ProductItem.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react'
+import style from "../styles/productItem.modules.css"
+
+export const ProductItem = ({ data }) => {
+    const [size, setSize] = useState(data.size[0])
+    const [index, setIndex] = useState(0)
+
+    const handleClick = (i) => {
+        setIndex(i)
+        setSize(data.size[i])
+    }
+
+    const handleFetch = e => {
+        console.log(e.target.id)
+    }
+
+    return (
+        <div className="container">
+            <div className="row text-secondary">
+                <div className="col-10">
+                    <small>
+                        {`Home > ${data.category} > ${data.subCategory} > ${data.brandName} > ${data.productName}`}
+                    </small>
+                </div>
+                <div className="col-2">
+                    Share On <img src="https://www.flaticon.com/svg/static/icons/svg/145/145802.svg" alt="faceBook" width="20px" />
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/145/145812.svg" className="ml-1" alt="Twitter" width="20px" />
+                    <img src="https://as2.ftcdn.net/jpg/02/76/20/05/500_F_276200518_j4wG6McWWgtJLaC9q4OxPRAfTdrbiMtH.jpg" className="ml-1" alt="Email" width="25px" />
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-3 border-right" onClick={(e) => handleFetch(e)}>
+                    <div className=" border-bottom mt-3">
+                        <span className="border-bottom border-success">Category</span>
+                    </div>
+                    <div>
+                        <small className={data.category === "Fruits and Vegetables" ? "text-success" : null}>Fruits and Vegetables</small>
+                        <div className="ml-3">
+                            <small id="Fresh vegetables" className={data.subCategory === "Fresh vegetables" ? "text-success" : null}>Fresh vegetable</small><br />
+                            <small id="Herbs and seasonings" className={data.subCategory === "Herbs and seasonings" ? "text-success" : null}>Herbs and seasonings</small><br />
+                            <small id="Fresh fruits" className={data.subCategory === "Fresh fruits" ? "text-success" : null}>Fresh fruits</small><br />
+                            <small id="Organic Fruits and Vegetable" className={data.subCategory === "Organic Fruits and Vegetable" ? "text-success" : null}>Organic fruits and vegetable</small>
+                        </div>
+                    </div>
+                    <div>
+                        <small className={data.category === "Foodgrains, oils & Masala" ? "text-success" : null}>Foodgrains Oil and Masala</small>
+                        <div className="ml-3">
+                            <small id="Rice & rice products" className={data.subCategory === "Rice & rice products" ? "text-success" : null}>Rice & rice products</small><br />
+                            <small id="Dry fruits" className={data.subCategory === "Dry fruits" ? "text-success" : null}>Dry fruits</small><br />
+                            <small id="Masala" className={data.subCategory === "Masala" ? "text-success" : null}>Masala</small><br />
+                            <small id="Salt, sugar & Jaggery" className={data.subCategory === "Salt, sugar & Jaggery" ? "text-success" : null}>Salt, sugar and Jaggery</small>
+                        </div>
+                    </div>
+                    <div>
+                        <small>Bakery , cakes and Dairy</small>
+                        <div className="ml-3">
+                            <small> Bread & bun</small><br />
+                            <small>Icecream and desert</small><br />
+                            <small> Cookies</small><br />
+                            <small>Bakery snacks</small>
+                        </div>
+                    </div>
+                    <div>
+                        <small>Beverage & Drinks</small>
+                        <div className="ml-3">
+                            <small>Healthy drink and supplements</small><br />
+                            <small>Tea & coffee</small><br />
+                            <small>Energy and soft drinks</small><br />
+                            <small>Fruit Juice</small>
+                        </div>
+                    </div>
+                    <div className=" border-bottom">
+                        <span className="border-bottom border-success">Brand</span>
+                    </div>
+                    <div>
+                        <small className="text-success">{data.brandName}</small>
+                    </div>
+                </div>
+                <div className="col-4 text-center  border-right">
+                    <img src={data.imageUrl} alt="productImage" width="90%" />
+                </div>
+                <div className="col-5 border-right mt-3">
+                    <small className="border-bottom border-dark">{data.brandName}</small>
+                    <h5 className="mt-1 mb-3">{data.productName} - {size}</h5>
+                    <small className="text-secondary">MRP.<em style={{ textDecoration: "line-through" }}>Rs. {data.mrp[index]}</em> <b className="text-dark">Price: Rs.{(data.mrp[index] - Math.floor(data.mrp[index] / data.offers))}</b> <b className="text-danger">you save : {data.offers}%</b> (including all taxes) </small><br />
+                    <span className="px-1 text-light" style={{ background: "#14a043" }}><small>{data.ratings}</small> <img src="https://www.flaticon.com/svg/static/icons/svg/1828/1828884.svg" alt="star" width="12px" /></span> {data.reviews} Reviews
+                    <div className="mt-3 mb-1">
+                        <button className="btn border-success">1</button>
+                        <button className="btn btn-success mx-3">ADD TO BASKET</button>
+                        <button className="btn border-success">SAVE</button>
+                    </div>
+                    <small className="text-secondary">Express: Today 5:00PM - 7:00PM</small>
+                    <div>
+                        <small>Pack Size</small>
+                        {
+                            data.size && data.size.map((item, i) => (
+                                <div key={i} className={`mt-3 p-2 rounded border ${index === i ? style.cards : ""}`} onClick={() => handleClick(i)}>
+                                    {item} <small className="text-secondary ml-5">Rs.{(data.mrp[i] - Math.floor(data.mrp[i] / data.offers))} MRP:<em style={{ textDecoration: "line-through" }}>Rs. {data.mrp[i]}</em> <b className="text-danger">{data.offers}% Off</b></small><span className="float-right"><img className={`${index === i ? "bg-success" : ""}`} src="https://www.flaticon.com/svg/static/icons/svg/594/594852.svg" alt="select" width="20px" /></span>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">
+                <h3>{data.productName} - {data.brandName}</h3>
+                <hr />
+                <p className="text-secondary">About Product</p>
+                <small>{data.description}</small>
+            </div>
+            <div className="my-4">
+                <h3>Rating and Reviews</h3>
+                <div className="bg-secondary p-1 text-light">
+                    <img src="https://www.flaticon.com/svg/static/icons/svg/2389/2389220.svg" alt="reviews" width="15px" /> Only customers who purchase this product from bigbasket can rate and review
+                </div>
+                <div className="row">
+                    <div className="col-3">
+                        <div className="border-left border-bottom px-5 py-3">
+                            <h2 className="text-success">{data.ratings} <small>Stars</small></h2>
+                            <small>{data.reviews} Reviews</small>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="col-9 border-left py-4">
+                        <h5>Product Reviews</h5>
+                        <hr />
+                        <div className="border">
+                            {
+                                data.reviewList && data.reviewList.map((item, i) => (
+                                    <div className="p-5 border">
+                                        <h5>{item.text}HEllow</h5>
+                                        <small className="text-secondary">{item.user}fasdlifjks</small>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
