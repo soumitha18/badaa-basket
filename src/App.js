277cdbe69f464ddbc9d6ac9cf8f3108cb8dcc20d
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-04 19:10:55 +0530

    Demo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react"
 import { NavBar } from "./Components/NavBar"
 import { Home } from "./Components/Pages/Home"
 import { Footer } from "./Components/Footer"
+import SignInAndSignUP from "./Components/SignInAndSignUp"
 // import { ProductItem } from "./Components/Pages/ProductItem"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <SignInAndSignUP />
+      {/* <Home /> */}
       {/* <ProductItem /> */}
       <Footer />
     </div>
