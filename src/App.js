a5018d3213871a5e13b7671c2aa85e2a31654091
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-04 18:56:55 +0530

    created th googleauth and Facebook Auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react"
-import { NavBar } from "./Components/NavBar"
-import { Home } from "./Components/Pages/Home"
-import { Footer } from "./Components/Footer"
-// import { ProductItem } from "./Components/Pages/ProductItem"
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      {/* <ProductItem /> */}
-      <Footer />
+
     </div>
   );
 }
