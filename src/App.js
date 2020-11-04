9c3504b0eee47bd5b65fcf9f4565d96e4fbf359b
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-04 09:50:41 +0530

    Modified Few Parts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react"
 import { NavBar } from "./Components/NavBar"
 import { Home } from "./Components/Pages/Home"
 import { Footer } from "./Components/Footer"
+// import { ProductItem } from "./Components/Pages/ProductItem"
 
 function App() {
   return (
     <div>
-
+      <NavBar />
+      <Home />
+      {/* <ProductItem /> */}
+      <Footer />
     </div>
   );
 }
