ddeb6e02df1514461f73241fe50e2a002f0fdeba
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-05 15:45:29 +0530

    Added SearchBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { NavBar } from "./Components/NavBar";
 import { Footer } from "./Components/Footer";
 import { Routes } from "./Components/Routes"
+import { Demo } from "./Components/Pages/demo";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Routes />
+      <Demo />
+      {/* <Routes /> */}
       <Footer />
     </div>
   );
