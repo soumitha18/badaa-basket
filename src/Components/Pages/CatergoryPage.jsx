3a92a7d911efe22bfdfcfe64b6c24b33dfdbead8
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-11-06 17:58:25 +0530

    dummy

--- src/Components/Pages/CatergoryPage.jsx
+++ src/Components/Pages/CatergoryPage.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import Axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { ProductCard } from './ProductCard'
 
